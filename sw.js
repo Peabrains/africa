b63e8cd29eef8ad094613f7f63e905820c880fa4
Peabrains <anonymousepea@gmail.com>
2026-08-07 13:59:10 +0800

    design-lab: bump cache version, precache account.js

--- sw.js
+++ sw.js
@@ -3,8 +3,8 @@
    Cache version bumped manually alongside APP_VERSION.
    BUILD: 202608070256
    ============================================================ */
-const CACHE   = 'africa-safari-platform-202608070445';
-const VERSION = '202608070445';
+const CACHE   = 'africa-safari-platform-202608070530';
+const VERSION = '202608070530';
 
 const PRECACHE = [
   './', './index.html', './css/tokens.css', './css/print.css',
@@ -18,6 +18,7 @@ const PRECACHE = [
   './js/screens/itinerary.js', './js/screens/map.js', './js/screens/dex.js',
   './js/screens/bookings.js', './js/screens/sos.js', './js/screens/stamps.js',
   './js/screens/food.js', './js/screens/landing.js', './js/screens/bucket-list.js',
+  './js/screens/account.js',
   './data/world-countries.geojson',
 ];
 
