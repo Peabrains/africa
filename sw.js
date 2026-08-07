f1c6d356503318c9f3b45a03256675184289492a
Peabrains <anonymousepea@gmail.com>
2026-08-07 18:59:01 +0800

    design-lab: bump cache version

--- sw.js
+++ sw.js
@@ -3,8 +3,8 @@
    Cache version bumped manually alongside APP_VERSION.
    BUILD: 202608070256
    ============================================================ */
-const CACHE   = 'africa-safari-platform-202608071200';
-const VERSION = '202608071200';
+const CACHE   = 'africa-safari-platform-202608071240';
+const VERSION = '202608071240';
 
 const PRECACHE = [
   './', './index.html', './css/tokens.css', './css/print.css',
