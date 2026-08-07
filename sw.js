0cab01b7c1d247bba324515eb39b51b65ecb2a2c
Peabrains <anonymousepea@gmail.com>
2026-08-07 14:15:04 +0800

    design-lab: bump cache version for header/nav fixes

--- sw.js
+++ sw.js
@@ -3,8 +3,8 @@
    Cache version bumped manually alongside APP_VERSION.
    BUILD: 202608070256
    ============================================================ */
-const CACHE   = 'africa-safari-platform-202608070530';
-const VERSION = '202608070530';
+const CACHE   = 'africa-safari-platform-202608070610';
+const VERSION = '202608070610';
 
 const PRECACHE = [
   './', './index.html', './css/tokens.css', './css/print.css',
