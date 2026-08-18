dd78dabc8e6391d3ee54374bbe7b55743221b38b
Qing de Kueh <qingdekueh@Afterthought.local>
2026-08-18 22:38:28 +0800

    Bump planner cache after syntax fix

--- sw.js
+++ sw.js
@@ -4,7 +4,7 @@
    BUILD: 202608180530
    ============================================================ */
 const CACHE   = 'africa-safari-design-lab-202608180530';
-const VERSION = '202608180915';
+const VERSION = '202608181000';
 
 const PRECACHE = [
   './', './index.html', './css/tokens.css', './css/print.css', './css/planner.css',
