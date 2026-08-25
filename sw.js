84a1858575dcf52f5bf7e094bfed33b0e4069464
Qing de Kueh <qingdekueh@Afterthought.local>
2026-08-25 11:51:23 +0800

    Refresh lab cache for photo carousel

--- sw.js
+++ sw.js
@@ -4,7 +4,7 @@
    BUILD: 202608180530
    ============================================================ */
 const CACHE   = 'africa-safari-design-lab-202608180530';
-const VERSION = '202608201000';
+const VERSION = '202608251200';
 
 const PRECACHE = [
   './', './index.html', './css/tokens.css', './css/print.css', './css/planner.css',
